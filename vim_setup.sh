5ce96eefb23dc54a34eb2b37dfeb6201934084dd
caseyparker <parker.casey@gmail.com>
2017-07-02 15:35:21 -0700

    modified:   vim_setup.sh 	Added vim-colors-solarized

--- vim_setup.sh
+++ vim_setup.sh
@@ -9,10 +9,13 @@ cd ~
 
 git clone https://github.com/souravchk/dotfiles.git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 
 cat ~/dotfiles/vimrc > ~/.vimrc 
 rm -rf ~/dotfiles
 
 echo "set encoding=utf-8 nobomb" >> ~/.vimrc 
+
+
 
 vim +PluginInstall +quall
