79f2f9ad1f57679273dc29fd32f7a81f47b3e048
caseyparker <parker.casey@gmail.com>
2017-07-02 15:15:24 -0700

    modified:   vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-echo off
+
 rm -f ~/.vimrc
 rm -rf ~/.vim
-echo on
-sudo apt install exuberant ctags -y
-git clone https://github.com/souravchk/dotfiles.git
-cat dotfiles/vimrc > ~/.vimrc
-echo "set encoding=utf-8 nobomb" >> ~/.vimrc
+rm -rf ~/vim
+rm -rf ~/dotfiles
 
-vim +PluginInstall +quall
+sudo apt install exuberant-ctags -y
+
+cd ~
+git clone https://github.com/souravchk/dotfiles.git
+cat dotfiles/vimrc > ~/.vimrc && echo "set encoding=utf-8 nobomb" >> ~/.vimrc &&vim +PluginInstall +quall
