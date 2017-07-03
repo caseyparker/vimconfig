4a54015d09585c4d17c97714da3882359cffd557
caseyparker <parker.casey@gmail.com>
2017-07-02 18:40:13 -0700

    modified:   vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -14,12 +14,11 @@ cat ~/dotfiles/vimrc > ~/.vimrc
 rm -rf ~/dotfiles
 
 # Disable some plugins I don't like much.
-sed  --i -e '/nerdtree/d' \
+sed  -i -e '/nerdtree/d' \
 	-e '/solarized/d' \
 	-e '/altercation/d' \
 	-e '/fazz/d' \
-	-e 's%schristoomey/vim-tmux-navigator%PProvost/vim-markdown-jekyll%' \
-	-i ~/.vimrc
+	-e 's%schristoomey/vim-tmux-navigator%PProvost/vim-markdown-jekyll%' ~/.vimrc
 
 echo "set encoding=utf-8 nobomb" >> ~/.vimrc 
 vim +PluginInstall +qall
