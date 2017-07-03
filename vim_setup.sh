5ee0c47766d0df0f0e3fb280b39a86805cb5b7cc
caseyparker <parker.casey@gmail.com>
2017-07-02 18:52:21 -0700

    modified:   vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -10,15 +10,16 @@ cd ~
 git clone https://github.com/souravchk/dotfiles.git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-cat ~/dotfiles/vimrc > ~/.vimrc 
-rm -rf ~/dotfiles
 
 # Disable some plugins I don't like much.
 sed  -i -e '/nerdtree/d' \
 	-e '/solarized/d' \
 	-e '/altercation/d' \
-	-e '/fazz/d' \
-	-e 's%schristoomey/vim-tmux-navigator%PProvost/vim-markdown-jekyll%' ~/.vimrc
+	-e '/flazz/d' \
+	-e 's|christoomey/vim-tmux-navigation|PPovost/vim-markdown-jekyll|' ~/dotfiles/vimrc
+
+cat ~/dotfiles/vimrc > ~/.vimrc 
+rm -rf ~/dotfiles
 
 echo "set encoding=utf-8 nobomb" >> ~/.vimrc 
 vim +PluginInstall +qall
