338fd22fa068ffef5df8253f9bc7f609affa5f1c
caseyparker <parker.casey@gmail.com>
2017-07-02 19:55:33 -0700

    modified:   vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -12,12 +12,15 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
 # Disable some plugins I don't like much.
-sed  -i -e 's/christoomey/PProvost/' \
-	-e 's/tmux-navigator/markdown-jekyll/' \
-	-e '/nerdtree/d' \
-	-e '/solarized/d' \
-	-e '/altercation/d' \
-	-e '/flazz/d' ~/dotfiles/vimrc\
+# Written stupidly to cope with weird sed versions.
+# Note: 'brew install gnu-sed' on OS X
+sed  -e 's/christoomey/PProvost/' \
+    -e 's/tmux-navigator/markdown-jekyll/' \
+    -e '/nerdtree/d' \
+    -e '/solarized/d' \
+    -e '/altercation/d' \
+    -e '/flazz/d' \
+    -e "s/'kchmck\/vim-coffee-script'/'leshill\/vim-json'/" -i ~/dotfiles/vimrc
 
 cat ~/dotfiles/vimrc > ~/.vimrc 
 rm -rf ~/dotfiles
