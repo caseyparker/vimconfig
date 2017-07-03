1ffa126eb46f17719f08456405a121c362e9afac
caseyparker <parker.casey@gmail.com>
2017-07-02 17:03:44 -0700

    modified:   vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -14,8 +14,8 @@ cat ~/dotfiles/vimrc > ~/.vimrc
 rm -rf ~/dotfiles
 
 # Disable some plugins I don't like much.
-sed	-e """s/Plugin 'scrooloose/nerdtree'/# Plugin 'scrooloose nerdtree'/""" \
-	-e """s/Plugin 'christoomey/vim-tmux-navigator'/# Plugin 'christoomey/vim-tmux-navigator'/""" \
+sed	-e '/nerdtree/c\# nerdtree' \
+	-e '/navigator/c\# vim-tmux-navigator' \
 	-i ~/.vimrc
 
 echo "set encoding=utf-8 nobomb" >> ~/.vimrc 
