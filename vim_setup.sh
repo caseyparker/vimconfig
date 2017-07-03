5539ae747a988b036d2a3ab35bbc6366134a58cf
caseyparker <parker.casey@gmail.com>
2017-07-02 17:21:24 -0700

    modified:   vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -11,7 +11,7 @@ git clone https://github.com/souravchk/dotfiles.git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 wget https://github.com/lifepillar/vim-solarized8/archive/v0.6.tar.gz \
-	&& tar -C ~/.vim/ -xvzf v0.6.tar.gz colors \
+	&& tar -xvff v0.6.tar.gz -C ~/.vim \
 	&& rm -f v0.6.tar.gz
 
 cat ~/dotfiles/vimrc > ~/.vimrc 
